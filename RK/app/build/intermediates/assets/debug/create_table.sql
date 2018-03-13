63e721f091d2ecbf93b44888c4950852ca634a95
enoatu <acro1452@gmail.com>
2018-03-13 16:40:49 +0900

    プレイアクティビティ修正

---
+++ RK/app/build/intermediates/assets/debug/create_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS story (
+id               int,
+subtext          text,
+maintext         text
+);
+
+CREATE TABLE IF NOT EXISTS character (
+id               int,
+img              int,
+subtext          text,
+maintext         text
+);
+
